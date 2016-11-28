d25d1e95356ffc40ed1f5dea9dfaf65626f5d6e4
David McHale <dmchale92@gmail.com>
2016-11-28 11:01:44 -0500

    Added Tests, separated functions, add config presets

--- icao_request.py
+++ icao_request.py
@@ -52,18 +52,31 @@ class ICAOSniffer(object):
                              "United Kingdom"]
 
     def __call__(self):
-        print(self.display_icao())
+        print("[Fibonacci-15-Even.py]-[TEST] - Running Unit Tests...")
+        self.test_retrieve_icao()
+        print('[Fibonacci-15-Even.py]-[TEST] - Unit Tests completed...')
+        return True
 
-    def retrieve_icao(self):
-        # This sets the data source
-        icao_data = requests.get(self.icao_data_url)
+    def retrieve_icao(self, country_list=None):
+        """
+        :return: list of ICAO codes based on country list input
+        """
+        if country_list is None:
+            country_list = self.country_list
 
         country_codes = []
 
-        soup = BeautifulSoup(icao_data.text)
-        # This iterates over the DOM of the data source and pulls out country codes based on input country, filtering out duplicate results
+        """
+        Set the data source
+        """
+        icao_data = requests.get(self.icao_data_url)
+        soup = BeautifulSoup(icao_data.text, "html.parser")
 
-        for country in self.country_list:
+        """
+        This iterates over the DOM of the data source and pulls out country codes
+        based on input country, filtering out duplicate results
+        """
+        for country in country_list:
             country_iteration = 0
             for tableData in soup.find_all(title=country):
                 if len(str(tableData.parent.parent.a.contents)) < 8 and country_iteration == 0:
@@ -76,3 +89,19 @@ class ICAOSniffer(object):
     def display_icao(self):
         for country in self.retrieve_icao():
             print(country)
+
+    def test_retrieve_icao(self):
+        country_list = self.retrieve_icao()
+        country_list.__len__()
+        try:
+            assert country_list.__len__() == 29
+            print('[icao_request.py]-[TEST] - [test_retrieve_icao] - PASS')
+            return True
+        except Exception as e:
+            print("[icao_request.py]-[TEST] - [test_retrieve_icao] - FAIL: " + str(e))
+            return False
+
+ICAOSniffer = ICAOSniffer()
+
+ICAOSniffer()
+
